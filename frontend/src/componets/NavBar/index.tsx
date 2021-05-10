04fd378be92ad25990baf1fef8aa52b47e5fbcb9
Rodrigo Longhi <rfslonghi@gmail.com>
2021-05-10 03:41:33 -0300

    Correcoes para o primeiro deploy

--- frontend/src/componets/NavBar/index.tsx
+++ frontend/src/componets/NavBar/index.tsx
@@ -6,13 +6,13 @@ function NavBar() {
                 <div className="collapse navbar-collapse" id="navbarCollapse">
                     <ul className="navbar-nav me-auto mb-2 mb-md-0">
                         <li className="nav-item">
-                            <a className="nav-link active">Apostadores</a>
+                            <a className="nav-link" href="/">Apostadores</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link active">Aposta</a>
+                            <a className="nav-link" href="/">Aposta</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link active">API</a>
+                            <a className="nav-link" href="/">API</a>
                         </li>
                     </ul>
                 </div>
