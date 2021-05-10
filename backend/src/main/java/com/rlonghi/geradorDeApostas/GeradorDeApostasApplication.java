0519ab2eb9b8f4b3739f2759d88fd0667de0a211
Rodrigo Longhi <rfslonghi@gmail.com>
2021-05-10 03:28:58 -0300

    Back-end reestruturado

---
+++ backend/src/main/java/com/rlonghi/geradorDeApostas/GeradorDeApostasApplication.java
@@ -0,0 +1,13 @@
+package com.rlonghi.geradorDeApostas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeradorDeApostasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeradorDeApostasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rlonghi/geradorDeApostas/GeradorDeApostasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rlonghi.geradorDeApostas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeradorDeApostasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
